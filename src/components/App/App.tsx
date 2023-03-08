8eb9cefca19eda06400afb23ce49d4d430c99685
Warlley Lopes <warlleylopes123@gmail.com>
2023-03-08 16:45:50 -0300

    add button component

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,11 +1,29 @@
+import Button from "../Button";
+import Header from "../Header";
 import "./App.css";
-import Header from "./Header";
+
+function TesteComponent() {
+  return (
+    <img
+      width="16px"
+      src="https://cdn-icons-png.flaticon.com/512/70/70866.png"
+      alt="img"
+    />
+  );
+}
 
 function App() {
   return (
     <div className="App">
       <Header title="AlgaStock" />
-      App
+      <div className="Conteiner">
+        <Button
+          onClick={() => window.alert("você clicou!")}
+          appendIcon={<TesteComponent />}
+        >
+          Alert
+        </Button>
+      </div>
     </div>
   );
 }
