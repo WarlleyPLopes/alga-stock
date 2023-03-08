0b0499e8d0ea553b351f6ff50bebf3128763fa56
Warlley Lopes <warlleylopes123@gmail.com>
2023-03-08 13:57:20 -0300

    clear whole application

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,27 +1,7 @@
-import TestComponents from "../TestComponent";
 import "./App.css";
-import logo from "./logo.svg";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <TestComponents name="Warlley" />
-
-        {/* <ClassComponent name="Mundo" /> */}
-
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div className="App">App</div>;
 }
 
 export default App;
