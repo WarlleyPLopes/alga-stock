1d086951e183d9f8bda1c8d6e8d84c373c47b4d9
Warlley Lopes <warlleylopes123@gmail.com>
2023-03-09 13:35:18 -0300

    add table html structure

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,4 +1,5 @@
 import Container from "../../shared/Container";
+import Table from "../../shared/Table";
 import Header from "../Header";
 import "./App.css";
 
@@ -7,7 +8,7 @@ function App() {
     <div className="App">
       <Header title="AlgaStock" />
       <Container>
-        
+        <Table/>
       </Container>
     </div>
   );
