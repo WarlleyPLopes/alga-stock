7c42c88d1630916d0100acf755c4dfc196ae0e9c
Warlley Lopes <warlleylopes123@gmail.com>
2023-03-09 21:16:02 -0300

    propify table

--- src/shared/Table/Table.tsx
+++ src/shared/Table/Table.tsx
@@ -1,61 +1,31 @@
-import Products from './Table.mockdata'
+import React from 'react'
+import organizeData from '../../utils/organizeDataForTable'
 import './Table.scss'
 
-const headers: TableHeader[] = [
-  { key: 'id', value: '#' },
-  { key: 'name', value: 'Product' },
-  { key: 'price', value: 'Price', right: true },
-  { key: 'stock', value: 'Available Stock', right: true }
-]
-
-declare interface TableHeader {
+export interface TableHeader {
   key: string
   value: string
   right?: boolean
 }
 
-type IndexedHeaders = {
-  [key: string]: TableHeader
+declare interface TableProps {
+  headers: TableHeader[]
+  data: any[]
+
+  eneableActions?: boolean
+
+  onDelete?: (item: any) => void
+  onDetail?: (item: any) => void
+  onEdit?: (item: any) => void
 }
 
-type OrganizedItem = {
-  [key: string]: any
-}
-
-function organizeData (data: any[], headers: TableHeader[]):
-  [OrganizedItem[], IndexedHeaders] {
-  const indexedHeaders: IndexedHeaders = {}
-
-  headers.forEach(header => {
-    indexedHeaders[header.key] = {
-      ...header
-    }
-  })
-
-  const headerKeysInOrder = Object.keys(indexedHeaders)
-
-  const organizedData = data.map(item => {
-    const organizedItem: OrganizedItem = {}
-
-    headerKeysInOrder.forEach(key => {
-      organizedItem[key] = item[key]
-    })
-
-    organizedItem.$original = item
-
-    return organizedItem
-  })
-
-  return [organizedData, indexedHeaders]
-}
-
-const Table = () => {
-  const [organizedData, indexedHeaders] = organizeData(Products, headers)
+const Table: React.FC<TableProps> = (props) => {
+  const [organizedData, indexedHeaders] = organizeData(props.data, props.headers)
   return <table className="AppTable">
     <thead>
       <tr>
         {
-          headers.map(header =>
+          props.headers.map(header =>
             <th
               className={header.right ? 'right' : ''}
               key={header.key}
@@ -76,11 +46,11 @@ const Table = () => {
                 .map((item, i) =>
                   item !== '$original'
                     ? <td
-                        key={row.$original.id + i}
-                        className={indexedHeaders[item].right ? 'right' : ''}
-                      >
-                        { row[item] }
-                      </td>
+                      key={row.$original.id + i}
+                      className={indexedHeaders[item].right ? 'right' : ''}
+                    >
+                      {row[item]}
+                    </td>
                     : null
                 )
             }
