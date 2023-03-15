4edf8b23a627acca5758297befee237d5fa1574f
Warlley Lopes <warlleylopes123@gmail.com>
2023-03-15 15:22:24 -0300

    add axios to the project

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./components/App";
 import "./index.css";
+import http from "./utils/http";
+
+http.get('/posts')
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
