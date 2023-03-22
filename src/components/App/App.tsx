ef5b5705e60e699805881eb04ee8e76eb32c3866
Warlley Lopes <warlleylopes123@gmail.com>
2023-03-22 17:15:45 -0300

    add not-found rote

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,6 +1,8 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import HomeView from '../../views/HomeView';
+import NotFoundView from '../../views/NotFoundView';
 import './App.css';
+
 
 function App() {
   return (
@@ -8,6 +10,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<HomeView />} />
+          <Route path="*" element={<NotFoundView />} />
         </Routes>
       </BrowserRouter>
     </div>
